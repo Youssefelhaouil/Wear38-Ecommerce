fe04265cc8d72e635fcd212cf826c1abf05054c4
Youssefelhaouil <youssefelha1949@gmail.com>
2024-11-29 15:23:28 +0100

    fix cover bg

--- src/Components/Home/Cover.tsx
+++ src/Components/Home/Cover.tsx
@@ -4,13 +4,16 @@ import Button from "../Ui/Button";
 function Cover() {
   return (
     <>
-      <div className="homeCover h-[200px] lg:h-[300px] flex flex-col justify-center items-center py-10 space-y-2 ">
+      <div
+        className="h-[200px] lg:h-[300px] flex flex-col justify-center items-center py-10 space-y-2 bg-center bg-no-repeat bg-cover"
+        style={{ backgroundImage: `url('./Cover.png')` }} 
+      >
         <h1 className="text-white text-xl lg:text-2xl xl:text-3xl xl:font-bold font-semibold">
           Enhance Your Everyday Style
         </h1>
         <p className="text-pretty text-sm lg:text-base text-light-gray/65 text-center">
           Upgrade your wardrobe with Wear 38. Our designs bring comfort and
-          confidence
+          confidence.
         </p>
         <Link to={"/products"}>
           <Button
